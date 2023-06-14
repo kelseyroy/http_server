8a14e724399cbee77d785adc496a2a38a528fa92
Kelsey Roy <kroy@8thlight.com>
2023-06-14 16:21:30 -0700

    Adds HEAD case in router.ex

--- lib/http_server_test_fixture/mock_handlers/mock_get.ex
+++ lib/http_server_test_fixture/mock_handlers/mock_get.ex
@@ -1,9 +1,16 @@
 defmodule HTTPServerTestFixture.Handlers.MockGet do
   alias HTTPServer.Request
   @behaviour HTTPServer.Handler
+  @body "this is a mocked get with a body"
 
   @impl HTTPServer.Handler
   def handle(%Request{method: "GET"} = _req) do
-    {200, "this is a mocked get with a body"}
+    {200, @body}
+  end
+  def handle(%Request{method: "HEAD"} = _req) do
+    {200, @body}
+  end
+  def handle(%Request{method: _method} = _req) do
+    {404, ""}
   end
 end
--- lib/router.ex
+++ lib/router.ex
@@ -3,6 +3,14 @@ defmodule HTTPServer.Router do
 
   def router(routes_fn, req) do
     {status_code, body} = routes_fn.(req)
-    Response.send_resp(status_code, body)
+    headers = Response.build_headers(body)
+
+    case req.method do
+      "HEAD" ->
+        Response.send_resp(status_code, "", headers)
+
+      _ ->
+        Response.send_resp(status_code, body, headers)
+    end
   end
 end
