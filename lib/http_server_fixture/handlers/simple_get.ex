1904607c267064153e7ddaceb8e7d6530d22626b
Kelsey Roy <kroy@8thlight.com>
2023-06-14 20:43:49 -0700

    Removes HEAD handlers and handles HEAD like a GET req

--- lib/http_server_fixture/handlers/simple_get.ex
+++ lib/http_server_fixture/handlers/simple_get.ex
@@ -7,10 +7,6 @@ defmodule HTTPServerFixture.SimpleGet do
     {200, ""}
   end
 
-  def handle(%Request{method: "HEAD"} = _req) do
-    {200, ""}
-  end
-
   def handle(%Request{method: _method} = _req) do
     {404, ""}
   end
--- lib/http_server_fixture/handlers/simple_head.ex
+++ lib/http_server_fixture/handlers/simple_head.ex
@@ -1,15 +1,12 @@
 defmodule HTTPServerFixture.SimpleHead do
   alias HTTPServer.Request
   @behaviour HTTPServer.Handler
-  @body "This body does not show up in a HEAD request"
 
   @impl HTTPServer.Handler
   def handle(%Request{method: "GET"} = _req) do
-    {200, @body}
+    {200, "This body does not show up in a HEAD request"}
   end
-  def handle(%Request{method: "HEAD"} = _req) do
-    {200, @body}
-  end
+
   def handle(%Request{method: _method} = _req) do
     {404, ""}
   end
