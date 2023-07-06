1e38cf7340b3903377ef0e62a98754266a92dece
Kelsey Roy <kroy@8thlight.com>
2023-07-06 10:16:58 -0700

    Adds add_static_dir/3 and helper funcs to ServeStatic

--- lib/router/handlers/serve_static.ex
+++ lib/router/handlers/serve_static.ex
@@ -1,25 +1,12 @@
 defmodule HTTPServer.Router.Handlers.ServeStatic do
   alias HTTPServer.Request
   alias HTTPServer.Router.Handlers.NotFound
-  require Logger
   @routes Application.compile_env(:http_server, :routes, Routes)
 
   def add_static_dir(routes, dirpath, path \\ "") do
-    filepaths = Path.wildcard(dirpath <> "/**")
-
-    Enum.map(filepaths, fn filepath ->
-      file_name = String.replace(filepath, dirpath, "")
-      new_path = path <> file_name
-
-      new_route = %{
-        handler: HTTPServer.Router.Handlers.ServeStatic,
-        methods: ["GET"],
-        filepath: filepath
-      }
-
-      {new_path, new_route}
-    end)
-    |> Map.new()
+    dirpath
+    |> get_filepaths()
+    |> build_routes(dirpath, path)
     |> Map.merge(routes)
   end
 
@@ -43,4 +30,24 @@ defmodule HTTPServer.Router.Handlers.ServeStatic do
       "css" -> :css
     end
   end
+
+  defp get_filepaths(dirpath), do: Path.wildcard(dirpath <> "/**")
+
+  defp build_routes(filepaths, dirpath, path) do
+    Enum.map(filepaths, fn filepath ->
+      {path(path, filepath, dirpath), route(filepath)}
+    end)
+    |> Map.new()
+  end
+
+  defp path(path, filepath, dirpath), do: path <> filename(filepath, dirpath)
+
+  defp filename(filepath, dirpath), do: String.replace(filepath, dirpath, "")
+
+  defp route(filepath),
+    do: %{
+      handler: HTTPServer.Router.Handlers.ServeStatic,
+      methods: ["GET"],
+      filepath: filepath
+    }
 end
