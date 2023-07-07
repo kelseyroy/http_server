81629da18b80bdb32fc13d79b932f2e51368dbcb
Kelsey Roy <kroy@8thlight.com>
2023-07-07 10:16:36 -0700

    Adds passing test for multiple static directory routes

--- test/http_serve_static_test.exs
+++ test/http_serve_static_test.exs
@@ -23,6 +23,7 @@ defmodule HTTPServerTest.ServeStatic do
         filepath: path <> "/mock-layout.css"
       }
     }
+
     assert ServeStatic.add_static_routes(%{}, path) == expected_routes
   end
 
@@ -46,14 +47,69 @@ defmodule HTTPServerTest.ServeStatic do
         filepath: path <> "/mock-layout.css"
       }
     }
+
     assert ServeStatic.add_static_routes(%{}, path, "/static") == expected_routes
   end
 
   test "Returns an empty map when filepath is incorrect, which will default to 404 not found when routed" do
-    path = "public"
+    path = "not_a_real_dir"
 
     empty_map = Map.new()
 
     assert ServeStatic.add_static_routes(%{}, path, "/static") == empty_map
+  end
+
+  test "Can add multiple static directories to routes map" do
+    path1 = "lib/http_server_test_fixture/mock_public"
+    path2 = "test/http_server_spec/web"
+
+    empty_map = Map.new()
+
+    mock_public_static_map = ServeStatic.add_static_routes(empty_map, path1)
+
+    expected_routes = %{
+      "/hello-world.txt" => %{
+        handler: HTTPServer.Router.Handlers.ServeStatic,
+        methods: ["GET"],
+        filepath: path1 <> "/hello-world.txt"
+      },
+      "/mock-html.html" => %{
+        handler: HTTPServer.Router.Handlers.ServeStatic,
+        methods: ["GET"],
+        filepath: path1 <> "/mock-html.html"
+      },
+      "/mock-layout.css" => %{
+        handler: HTTPServer.Router.Handlers.ServeStatic,
+        methods: ["GET"],
+        filepath: path1 <> "/mock-layout.css"
+      },
+      "/static/doggo.png" => %{
+        handler: HTTPServer.Router.Handlers.ServeStatic,
+        methods: ["GET"],
+        filepath: path2 <> "/doggo.png"
+      },
+      "/static/health-check.html" => %{
+        handler: HTTPServer.Router.Handlers.ServeStatic,
+        methods: ["GET"],
+        filepath: path2 <> "/health-check.html"
+      },
+      "/static/kisses.gif" => %{
+        handler: HTTPServer.Router.Handlers.ServeStatic,
+        methods: ["GET"],
+        filepath: path2 <> "/kisses.gif"
+      },
+      "/static/kitteh.jpg" => %{
+        handler: HTTPServer.Router.Handlers.ServeStatic,
+        methods: ["GET"],
+        filepath: path2 <> "/kitteh.jpg"
+      },
+      "/static/layout-styles.css" => %{
+        handler: HTTPServer.Router.Handlers.ServeStatic,
+        methods: ["GET"],
+        filepath: path2 <> "/layout-styles.css"
+      }
+    }
+
+    assert ServeStatic.add_static_routes(mock_public_static_map, path2, "/static") == expected_routes
   end
 end
