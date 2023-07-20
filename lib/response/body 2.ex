a814e29434d2f678e50df07aefad3ffd17f3928f
Kelsey Roy <kroy@8thlight.com>
2023-07-20 09:20:38 -0700

    Adds DELETE to-do handler

--- lib/response/body 2.ex
+++
@@ -1,5 +0,0 @@
-defmodule HTTPServer.Response.Body do
-  def handle(body, :json) do
-    JSON.decode!(body)
-  end
-end
--- lib/to_do/routes.ex
+++ lib/to_do/routes.ex
@@ -2,11 +2,7 @@ defmodule ToDo.Routes do
   def routes, do: %{
     "/todo" => %{
       handler: ToDo.Handlers.ToDo,
-      methods: ["POST"]
-    },
-    # "/todo/#{is_integer()}" => %{
-    #   handler: ToDo.Handlers.ToDo,
-    #   methods: ["POST"]
-    # }
+      methods: ["POST", "DELETE"]
+    }
   }
 end
